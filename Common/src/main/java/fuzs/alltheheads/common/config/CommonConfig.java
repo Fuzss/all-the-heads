699d26ec7830201e05822a1e55cf2c2003af9bab
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-05-24 00:18:51 +0200

    mark config setting correctly

--- Common/src/main/java/fuzs/alltheheads/common/config/CommonConfig.java
+++ Common/src/main/java/fuzs/alltheheads/common/config/CommonConfig.java
@@ -4,6 +4,6 @@ import fuzs.puzzleslib.common.api.config.v3.Config;
 import fuzs.puzzleslib.common.api.config.v3.ConfigCore;
 
 public class CommonConfig implements ConfigCore {
-    @Config(description = "Drop vanilla mob heads with the same rate as wither skeleton skulls.")
+    @Config(description = "Drop vanilla mob heads with the same rate as wither skeleton skulls.", worldRestart = true)
     public boolean vanillaHeadDrops = true;
 }
